9b6b04303085906c2ac154c7f0a07bade8fc8f90
Kelukin_Lau <Kelukin@gmail.com>
2018-09-27 18:58:19 +0800

    Add Annotation

--- login_wlrz.py
+++ login_wlrz.py
@@ -2,7 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 def login_wlrz(username, pwd):
-    driver = webdriver.Edge()
+    //当前脚本使用selenium，模拟人手动登录的过程
+    driver = webdriver.Edge()//当前默认使用的是Edge浏览器
+    //driver = webdriver.Chrome()//浏览器不同也对应着替换，注意的是放到Path下的driver也必须不同
+    //driver = webdriver.FireFox()
     driver.get("http://wlrz.fudan.edu.cn")
     elem = driver.find_element_by_name("username")
     elem.clear()
