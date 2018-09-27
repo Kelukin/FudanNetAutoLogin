324530c31962ad8b6b2e79cff3491c3856ec698b
Kelukin_Lau <Kelukin@gmail.com>
2018-09-27 18:55:37 +0800

    Add annotation

--- login_main.py
+++ login_main.py
@@ -3,8 +3,8 @@ from login_wlrz import login_wlrz
 import time
 lastStatus = True
 while True:
-    lastStatus = checkIfCanConnect(lastStatus)
+    lastStatus = checkIfCanConnect(lastStatus)//定时检查当前网络是否连接
     if lastStatus:
-        time.sleep(10)
+        time.sleep(10)//10秒后开启下一轮询问
     else:
-        login_wlrz('18210240012','305471')
+        login_wlrz('yourUserName','yourPassword') //将函数里面对应的参数替换为你的学号以及密码
