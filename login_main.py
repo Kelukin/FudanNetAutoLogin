45a26c85acbc8118e8cf3cb5751a4f476954e8c5
Kelukin_Lau <Kelukin@gmail.com>
2018-09-27 19:00:34 +0800

    Update login_main.py

--- login_main.py
+++ login_main.py
@@ -3,8 +3,8 @@ from login_wlrz import login_wlrz
 import time
 lastStatus = True
 while True:
-    lastStatus = checkIfCanConnect(lastStatus)//定时检查当前网络是否连接
+    lastStatus = checkIfCanConnect(lastStatus)#定时检查当前网络是否连接
     if lastStatus:
         time.sleep(10)//10秒后开启下一轮询问
     else:
-        login_wlrz('yourUserName','yourPassword') //将函数里面对应的参数替换为你的学号以及密码
+        login_wlrz('yourUserName','yourPassword') #将函数里面对应的参数替换为你的学号以及密码
